ed76cb500a3fa870919167eb8e1885699a0c86ee
Daniel <drei509@aucklanduni.ac.nz>
2023-12-15 12:20:34 +1300

    Implemented model for goehrin et al. 2011

--- src/models/goehring.py
+++ src/models/goehring.py
@@ -1,9 +1,131 @@
 # Based on Goehring et al. 2011
+from typing import Callable
+import numpy as np
+from scipy import integrate
+
+def default_v_func(kvals, x,t):
+    if t > 1000:
+        return 0*x
+    else:
+        return -x*(x-190)*(x-35) / (700000*(np.maximum(1,np.abs((t-120)/80)))**2)
+
+
+Ybar = lambda kvals, Y: 2 * integrate.simpson(Y, kvals["X"]) / kvals["L"]  # handles both A-bar and P-bar
+default_A_cyto = lambda kvals, A: kvals["rho_A"] - kvals["psi"] * Ybar(kvals, A)
+default_P_cyto = lambda kvals, P: kvals["rho_P"] - kvals["psi"] * Ybar(kvals, P)
 
 DEFAULT_PARAMETERS = {
+    "label": "goehring",
+    "points_per_second": 2,
 
+    # General Setup Variables
+    "Nx": 100,  # number of length steps
+    "L": 134.6,  # length of region
+    "x0": 0,
+    "xL": 67.3,  # L / 2
+    "t0": 0,
+    "tL": 5000,
 
+    # Model parameters and functions
+    "psi": 0.174,
+    "D_A": 0.28,
+    "D_P": 0.15,
+    "k_onA": 8.58 * 10 ** (-3),
+    "k_onP": 4.74 * 10 ** (-2),  # (TODO) 10^-3 or 10^-2 ?? (is 2 in the paper)
+    "k_offA": 5.4 * 10 ** (-3),
+    "k_offP": 7.3 * 10 ** (-3),
+    "k_AP": 0.190,
+    "k_PA": 2.0,
+    "rho_A": 1.56,
+    "rho_P": 1.0,
+
+    "alpha": 1,
+    "beta": 2,
+
+    # R_X
+    # Xbar
+    "A_cyto": default_A_cyto,
+    "P_cyto": default_P_cyto,
+    "v_func": default_v_func,
 }
 
-def run_model(args):
-    pass
+
+def discrete_diffusion_term(kvals: dict, Y, x_i):
+    # This function accounts for boundary reflection
+    if x_i == 0:  # left boundary
+        return (Y[1] - 2 * Y[0] + Y[1]) / kvals["deltax"] ** 2  # reflect Y[-1] to Y[1]
+    elif x_i == kvals["Nx"] - 1:  # right boundary
+        return (Y[kvals["Nx"] - 2] - 2 * Y[kvals["Nx"] - 1] + Y[kvals["Nx"] - 2]) / kvals[
+            "deltax"] ** 2  # reflect Y[Nx] over Nx-1 to Y[Nx-2]
+    else:  # internal point
+        return (Y[x_i + 1] - 2 * Y[x_i] + Y[x_i - 1]) / kvals["deltax"] ** 2
+
+
+# where func is a function of type x_i -> float
+def discrete_spatial_derivative(kvals: dict, func: Callable[[int], float], x_i):
+    return (func(x_i + 1) - func(x_i)) / kvals["deltax"]
+
+R_A = lambda kvals, A, P, A_cyto_r, t, x_i: kvals["k_onA"] * A_cyto_r \
+    - kvals["k_offA"] * A[x_i] - kvals["k_AP"] * (P[x_i] ** kvals["alpha"]) * A[x_i]
+R_P = lambda kvals, A, P, P_cyto_r, t, x_i: kvals["k_onP"] * P_cyto_r \
+    - kvals["k_offP"] * P[x_i] - kvals["k_PA"] * (A[x_i] ** kvals["beta"]) * P[x_i]
+
+
+def odefunc(t, U, kvals):
+    assert len(U) == 2 * kvals["Nx"]
+
+    A = U[:kvals["Nx"]]
+    P = U[kvals["Nx"]:]
+
+    dudt_A = np.zeros(kvals["Nx"])
+    dudt_P = np.zeros(kvals["Nx"])
+
+    # r is for "resolved"
+    A_cyto_r = kvals["A_cyto"](kvals, A)
+    P_cyto_r = kvals["P_cyto"](kvals, P)
+
+    # manually handle right boundary ( x_i = Nx-1 ) since v(x,t)
+    # reflect Nx over Nx-1 to Nx-2; for v_func, also negate on the reflection as v(x)=-v(-x)
+    dudt_A[kvals["Nx"]-1] = kvals["D_A"] * discrete_diffusion_term(kvals, A, kvals["Nx"]-1) \
+        - (kvals["v_func"](kvals, kvals["X"][kvals["Nx"]-2], t) * A[kvals["Nx"]-2] - kvals["v_func"](kvals, kvals["X"][kvals["Nx"]-1], t) * A[kvals["Nx"]-1]) / kvals["deltax"] \
+        + R_A(kvals, A, P, A_cyto_r, t, kvals["Nx"]-1)
+    dudt_P[kvals["Nx"]-1] = kvals["D_P"] * discrete_diffusion_term(kvals, P, kvals["Nx"]-1) \
+        - (kvals["v_func"](kvals, kvals["X"][kvals["Nx"]-2], t) * P[kvals["Nx"]-2] - kvals["v_func"](kvals, kvals["X"][kvals["Nx"]-1], t) * P[kvals["Nx"]-1]) / kvals["deltax"] \
+        + R_P(kvals, A, P, P_cyto_r, t, kvals["Nx"]-1)
+
+    # insides
+    # diffusion function handles left boundary
+    for x_i in np.arange(0, kvals["Nx"] - 1):
+        dudt_A[x_i] = kvals["D_A"] * discrete_diffusion_term(kvals, A, x_i) \
+              - discrete_spatial_derivative(kvals, lambda x_ii: kvals["v_func"](kvals, kvals["X"][x_ii], t) * A[x_ii], x_i) \
+              + R_A(kvals, A, P, A_cyto_r, t, x_i)
+        dudt_P[x_i] = kvals["D_P"] * discrete_diffusion_term(kvals, P, x_i) \
+              - discrete_spatial_derivative(kvals, lambda x_ii: kvals["v_func"](kvals, kvals["X"][x_ii], t) * P[x_ii], x_i) \
+              + R_P(kvals, A, P, P_cyto_r, t, x_i)
+
+    return np.ravel([dudt_A, dudt_P])
+
+
+def run_model(args: dict = {}):
+    params = {**DEFAULT_PARAMETERS, **args}
+
+    # calculate other widely used values
+    X = np.linspace(params["x0"], params["xL"], params["Nx"])
+    deltax = np.abs(X[1] - X[0])
+
+    # key values
+    kvals: dict = {**params, "X": X, "deltax": deltax}
+
+    # default time points for solver output
+    kvals["t_eval"] = kvals["t_eval"] if "t_eval" in kvals else np.linspace(kvals["t0"], kvals["tL"],
+                                                                            kvals["points_per_second"] * np.abs(
+                                                                                kvals["tL"] - kvals["t0"]))
+
+    # default initial condition if none passed
+    kvals["initial_condition"] = kvals["initial_condition"] if "initial_condition" in kvals else np.ravel([[0.5, 0.0] for x_i in np.arange(0, kvals["Nx"])], order='F')
+
+    sol = integrate.solve_ivp(odefunc, [kvals["t0"], kvals["tL"]], kvals["initial_condition"], method="BDF",
+                              t_eval=kvals["t_eval"], args=(kvals,))
+
+    return sol, kvals
+
