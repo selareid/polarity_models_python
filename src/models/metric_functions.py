6ad05905b698b9e8bb00b29d60077adaa234d345
Daniel <drei509@aucklanduni.ac.nz>
2024-01-25 10:35:20 +1300

    Added better-optimised metric function. Refactored metric functions to be more centralised.

--- src/models/metric_functions.py
+++ src/models/metric_functions.py
@@ -1,15 +1,12 @@
 import numpy as np
 from scipy import integrate
 
+
 # Analysis Metric
 # A,P just on membrane
 def polarity_measure(X, Am, Pm, Nx):
-    a_left = integrate.simpson(Am[:Nx//2], X[:Nx//2])
-    a_right = integrate.simpson(Am[Nx//2:], X[Nx//2:])
-    p_left = integrate.simpson(Pm[:Nx//2], X[:Nx//2])
-    p_right = integrate.simpson(Pm[Nx//2:], X[Nx//2:])
-
-    return 0 if ((a_left + a_right)*(p_left + p_right)) == 0 else np.abs(a_left - a_right) * np.abs(p_left - p_right) / ((a_left + a_right)*(p_left + p_right))
+    measure, _, _ = polarity_get_all(X, Am, Pm, Nx)
+    return measure
 
 
 # determine the orientation of polarity
@@ -17,18 +14,30 @@ def polarity_measure(X, Am, Pm, Nx):
 # 1 - anterior on left
 # 2 - anterior on right
 def polarity_orientation(X, Am, Pm, Nx):
-    a_left = integrate.simpson(Am[:Nx // 2], X[:Nx // 2])
-    a_right = integrate.simpson(Am[Nx // 2:], X[Nx // 2:])
-    p_left = integrate.simpson(Pm[:Nx // 2], X[:Nx // 2])
-    p_right = integrate.simpson(Pm[Nx // 2:], X[Nx // 2:])
-
-    if a_left > a_right and p_right > p_left:  # A is on the left, P is on the right
-        return 1
-    elif a_left < a_right and p_right < p_left:  # A is on the right, P is on the left
-        return 2
-    else:  # undetermined
-        return 0
+    _, orientation, _ = polarity_get_all(X, Am, Pm, Nx)
+    return orientation
 
 
 def orientation_marker(orientation_code):
     return ['o', '<', '>'][orientation_code]
+
+
+# returns (measure, orientation, marker)
+def polarity_get_all(X, Am, Pm, Nx):
+    a_left = integrate.simpson(Am[:Nx//2], X[:Nx//2])
+    a_right = integrate.simpson(Am[Nx//2:], X[Nx//2:])
+    p_left = integrate.simpson(Pm[:Nx//2], X[:Nx//2])
+    p_right = integrate.simpson(Pm[Nx//2:], X[Nx//2:])
+
+    measure = 0 if ((a_left + a_right)*(p_left + p_right)) == 0 else np.abs(a_left - a_right) * np.abs(p_left - p_right) / ((a_left + a_right)*(p_left + p_right))
+
+    # Orientation
+    if a_left > a_right and p_right > p_left:  # A is on the left, P is on the right
+        orientation = 1
+    elif a_left < a_right and p_right < p_left:  # A is on the right, P is on the left
+        orientation = 2
+    else:  # undetermined
+        orientation = 0
+
+    return measure, orientation, orientation_marker(orientation)
+
