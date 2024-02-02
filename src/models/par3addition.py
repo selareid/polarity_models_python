e40f1593a24f3940e4a4d7cda4d6687cb89e69f6
Daniel <drei509@aucklanduni.ac.nz>
2024-02-02 15:35:43 +1300

    New and improved 4-equation par3 model

--- src/models/par3addition.py
+++ src/models/par3addition.py
@@ -20,9 +20,11 @@ def default_v_func(kvals, x, t):
 
 
 Ybar = lambda kvals, Y: 2 * integrate.simpson(Y, kvals["X"]) / kvals["L"]  # all of J,A,P-bar
-def default_J_cyto(kvals, J): return kvals["rho_J"] - kvals["psi"] * Ybar(kvals, J)
-def default_A_cyto(kvals, A): return kvals["rho_A"] - kvals["psi"] * Ybar(kvals, A)
-def default_P_cyto(kvals, P): return kvals["rho_P"] - kvals["psi"] * Ybar(kvals, P)
+def J_cyto(kvals, J, M): return kvals["rho_J"] - kvals["psi"] * Ybar(kvals, J) \
+        - kvals["psi"] * Ybar(kvals, M)
+def A_cyto(kvals, A, M): return kvals["rho_A"] - kvals["psi"] * Ybar(kvals, A) \
+        - kvals["psi"] * Ybar(kvals, M)
+def P_cyto(kvals, P): return kvals["rho_P"] - kvals["psi"] * Ybar(kvals, P)
 
 
 DEFAULT_PARAMETERS = {
@@ -42,36 +44,40 @@ DEFAULT_PARAMETERS = {
 
     # diffusion coefficients
     "D_J": 0.28,
+    "D_M": 0.28,
     "D_A": 0.28,
     "D_P": 0.15,
 
     # velocity coefficients
-    "sigma_J": 1,
-    "sigma_P": 1,
+    "sigmaJ": 1,
+    "sigmaM": 1,
+    "sigmaP": 1,
 
-    "k_onJ": 8.58 * 10 ** (-3),  # value taken from A equivalent
-    "k_onP": 4.74 * 10 ** (-2),
+    # aPar complex change coefficients
+    "k1": 8.58 * 10**(-3),
+    "k2": 6.5 * 10**(-3),
 
-    "k_offA": 5.4 * 10 ** (-3),
-    "k_offP": 7.3 * 10 ** (-3),
+    # antagonism
+    "kJP": 0.190,
+    "kPA": 2.0,
 
-    "k_JP": 0.190,  # values taken from k_AP
-    "k_AJ": 0.190,  # values taken from k_AP
-    "k_PA": 2.0,
-
-    "rho_J": 1.56,
-    "rho_A": 1.56,
-    "rho_P": 1.0,
-
-    "gamma": 1,
+    # antagonism exponents
     "alpha": 1,
     "beta": 2,
 
-    # R_X
-    # Xbar
-    "J_cyto": default_J_cyto,
-    "A_cyto": default_A_cyto,
-    "P_cyto": default_P_cyto,
+    # total amount in system
+    "rho_A": 1.56,
+    "rho_J": 1.56,
+    "rho_P": 1.0,
+
+    "konJ": 8.58 * 10**(-3),  # TODO
+    "konA": 0,
+    "konP": 4.74 * 10**(-2),
+
+    "koffJ": 7.3 * 10**(-3),
+    "koffA": 5.4 * 10**(-3),
+    "koffP": 7.3 * 10**(-3),
+
     "v_func": default_v_func,
 }
 
@@ -92,15 +98,19 @@ def disc_spatial_derivative(kvals: dict, func: Callable[[int], float], x_i):
     return (func(x_i + 1) - func(x_i)) / kvals["deltax"]
 
 
-R_J = lambda kvals, J, A, P, J_cyto_r, t, x_i: kvals["k_onJ"] * J_cyto_r - kvals["k_JP"]*(P[x_i]**kvals["gamma"])*J[x_i]
-R_A = lambda kvals, J, A, P, A_cyto_r, t, x_i: -kvals["k_offA"]*A[x_i] \
-    + kvals["k_AJ"] * (J[x_i] ** kvals["alpha"]) * A_cyto_r
-R_P = lambda kvals, J, A, P, P_cyto_r, t, x_i: kvals["k_onP"] * P_cyto_r \
-    - kvals["k_offP"] * P[x_i] - kvals["k_PA"] * (A[x_i] ** kvals["beta"]) * P[x_i]
+R_J = lambda kvals, J, M, A, P, t, x_i, A_cyto_r, J_cyto_r: -kvals["k1"]*A_cyto_r*J[x_i] + kvals["k2"]*M[x_i] \
+                                                    + kvals["konJ"]*J_cyto_r - kvals["koffJ"]*J[x_i] \
+                                                    - kvals["kJP"]*P[x_i]**kvals["alpha"]*J[x_i]
+R_M = lambda kvals, J, M, A, P, t, x_i, A_cyto_r: kvals["k1"]*A_cyto_r*J[x_i] - kvals["k2"]*M[x_i]
+R_A = lambda kvals, J, M, A, P, t, x_i, A_cyto_r: kvals["k2"]*M[x_i] + kvals["konA"]*A_cyto_r - kvals["koffA"]*A[x_i]
+R_P = lambda kvals, J, M, A, P, t, x_i, P_cyto_r: kvals["konP"]*P_cyto_r - kvals["koffP"]*P[x_i] \
+                                                    - kvals["kPA"]*(A[x_i]+M[x_i])**kvals["beta"]*P[x_i]
 
 
 def odefunc(t, U, kvals):
-    assert len(U) == 3 * kvals["Nx"]
+    Nx = kvals["Nx"]
+
+    assert len(U) == 4 * Nx
 
     # Failure so odefunc doesn't run forever trying to fix numerical issues
     if min(U) < -100 or max(U) > 100:
@@ -108,43 +118,52 @@ def odefunc(t, U, kvals):
         # plot_failure(U, t, kvals)
         raise AssertionError
 
-    J = U[:kvals["Nx"]]
-    A = U[kvals["Nx"]:2*kvals["Nx"]]
-    P = U[2*kvals["Nx"]:]
+    J = U[:Nx]
+    M = U[Nx:2*Nx]
+    A = U[2*Nx:3*Nx]
+    P = U[3*Nx:]
 
-    dudt_J = [0] * kvals["Nx"]
-    dudt_A = [0] * kvals["Nx"]
-    dudt_P = [0] * kvals["Nx"]
+    dudt_J = [0]*Nx
+    dudt_M = [0]*Nx
+    dudt_A = [0]*Nx
+    dudt_P = [0]*Nx
 
     # r is for "resolved"
-    J_cyto_r = kvals["J_cyto"](kvals, J)
-    A_cyto_r = kvals["A_cyto"](kvals, A)
-    P_cyto_r = kvals["P_cyto"](kvals, P)
-
-    # manually handle right boundary ( x_i = Nx-1 ) since v(x,t) is odd
-    # reflect Nx over Nx-1 to Nx-2; for v_func, also negate on the reflection as v(x)=-v(-x)
-    dudt_J[kvals["Nx"]-1] = kvals["D_J"] * disc_diffusion_term(kvals, J, kvals["Nx"]-1) \
-        - (-kvals["v_func"](kvals, kvals["X"][kvals["Nx"]-2], t) * J[kvals["Nx"]-2] - kvals["v_func"](kvals, kvals["X"][kvals["Nx"]-1], t) * J[kvals["Nx"]-1]) / kvals["deltax"] \
-        + R_J(kvals, J, A, P, J_cyto_r, t, kvals["Nx"]-1)
-    dudt_A[kvals["Nx"]-1] = kvals["D_A"] * disc_diffusion_term(kvals, A, kvals["Nx"]-1) \
-        + R_A(kvals, J, A, P, A_cyto_r, t, kvals["Nx"]-1)
-    dudt_P[kvals["Nx"]-1] = kvals["D_P"] * disc_diffusion_term(kvals, P, kvals["Nx"]-1) \
-        - (-kvals["v_func"](kvals, kvals["X"][kvals["Nx"]-2], t) * P[kvals["Nx"]-2] - kvals["v_func"](kvals, kvals["X"][kvals["Nx"]-1], t) * P[kvals["Nx"]-1]) / kvals["deltax"] \
-        + R_P(kvals, J, A, P, P_cyto_r, t, kvals["Nx"]-1)
+    J_cyto_r = J_cyto(kvals, J, M)
+    A_cyto_r = A_cyto(kvals, A, M)
+    P_cyto_r = P_cyto(kvals, P)
 
     # insides
     # diffusion function handles left boundary
-    for x_i in np.arange(0, kvals["Nx"] - 1):
-        dudt_J[x_i] = kvals["D_J"] * disc_diffusion_term(kvals, J, x_i) \
-              - kvals["sigma_J"] * disc_spatial_derivative(kvals, lambda x_ii: kvals["v_func"](kvals, kvals["X"][x_ii], t) * J[x_ii], x_i) \
-              + R_J(kvals, J, A, P, J_cyto_r, t, x_i)
-        dudt_A[x_i] = kvals["D_A"] * disc_diffusion_term(kvals, A, x_i) \
-              + R_A(kvals, J, A, P, A_cyto_r, t, x_i)
-        dudt_P[x_i] = kvals["D_P"] * disc_diffusion_term(kvals, P, x_i) \
-              - kvals["sigma_P"] * disc_spatial_derivative(kvals, lambda x_ii: kvals["v_func"](kvals, kvals["X"][x_ii], t) * P[x_ii], x_i) \
-              + R_P(kvals, J, A, P, P_cyto_r, t, x_i)
+    for x_i in np.arange(0, Nx-1):
+        dudt_J[x_i] = kvals["D_J"]*disc_diffusion_term(kvals, J, x_i) \
+                        -kvals["sigmaJ"]*disc_spatial_derivative(kvals, lambda x_ii: kvals["v_func"](kvals, kvals["X"][x_ii], t)*J[x_ii], x_i) \
+                        + R_J(kvals, J, M, A, P, t, x_i, A_cyto_r, J_cyto_r)
+        dudt_M[x_i] = kvals["D_M"]*disc_diffusion_term(kvals, M, x_i) \
+                        -kvals["sigmaM"]*disc_spatial_derivative(kvals, lambda x_ii: kvals["v_func"](kvals, kvals["X"][x_ii], t)*M[x_ii], x_i) \
+                        + R_M(kvals, J, M, A, P, t, x_i, A_cyto_r)
+        dudt_A[x_i] = kvals["D_A"]*disc_diffusion_term(kvals, A, x_i) \
+                        + R_A(kvals, J, M, A, P, t, x_i, A_cyto_r)
+        dudt_P[x_i] = kvals["D_P"]*disc_diffusion_term(kvals, P, x_i) \
+                        -kvals["sigmaP"]*disc_spatial_derivative(kvals, lambda x_ii: kvals["v_func"](kvals, kvals["X"][x_ii], t)*P[x_ii], x_i) \
+                        + R_P(kvals, J, M, A, P, t, x_i, P_cyto_r)
 
-    return dudt_J + dudt_A + dudt_P
+    # manually handle right boundary ( x_i = Nx-1 ) since v(x,t) is odd
+    # reflect Nx over Nx-1 to Nx-2; for v_func, also negate on the reflection as v(x)=-v(-x)
+    x_i = Nx-1
+    dudt_J[x_i] = kvals["D_J"]*disc_diffusion_term(kvals, J, x_i) \
+                    - (-kvals["v_func"](kvals, kvals["X"][Nx-2], t)*J[Nx-2] - kvals["v_func"](kvals, kvals["X"][Nx-1], t)*J[Nx-1]) / kvals["deltax"] \
+                    + R_J(kvals, J, M, A, P, t, x_i, A_cyto_r, J_cyto_r)
+    dudt_M[x_i] = kvals["D_M"]*disc_diffusion_term(kvals, M, x_i) \
+                    - (-kvals["v_func"](kvals, kvals["X"][Nx-2], t)*M[Nx-2] - kvals["v_func"](kvals, kvals["X"][Nx-1], t)*M[Nx-1]) / kvals["deltax"] \
+                    + R_M(kvals, J, M, A, P, t, x_i, A_cyto_r)
+    dudt_A[x_i] = kvals["D_A"]*disc_diffusion_term(kvals, A, x_i) \
+                    + R_A(kvals, J, M, A, P, t, x_i, A_cyto_r)
+    dudt_P[x_i] = kvals["D_P"]*disc_diffusion_term(kvals, P, x_i) \
+                    - (-kvals["v_func"](kvals, kvals["X"][Nx-2], t)*P[Nx-2] - kvals["v_func"](kvals, kvals["X"][Nx-1], t)*P[Nx-1]) / kvals["deltax"] \
+                    + R_P(kvals, J, M, A, P, t, x_i, P_cyto_r)
+
+    return dudt_J + dudt_M + dudt_A + dudt_P
 
 
 def run_model(args=None):
@@ -163,7 +182,7 @@ def run_model(args=None):
     kvals["t_eval"] = kvals["t_eval"] if "t_eval" in kvals else np.linspace(kvals["t0"], kvals["tL"], int(kvals["points_per_second"] * np.abs(kvals["tL"] - kvals["t0"])))
 
     # default initial condition (just all 0) if none passed
-    kvals["initial_condition"] = kvals["initial_condition"] if "initial_condition" in kvals else [0]*(kvals["Nx"]*3)
+    kvals["initial_condition"] = kvals["initial_condition"] if "initial_condition" in kvals else [0]*(kvals["Nx"]*4)
 
     sol = integrate.solve_ivp(odefunc, [kvals["t0"], kvals["tL"]], kvals["initial_condition"], method="BDF",
                               t_eval=kvals["t_eval"], args=(kvals,))
@@ -180,13 +199,16 @@ def animate_plot(sol, kvals: dict, save_file=False, file_code: str = None, resca
     scalar = 1 if not rescale else np.max(sol.y)
     v_rescale_for_visibility = np.max(sol.y)/scalar * 10  # rescale so 0.1 is equal to max protein quantity in the plotting of v
 
-    # J = U[:kvals["Nx"]]
-    # A = U[kvals["Nx"]:2 * kvals["Nx"]]
-    # P = U[2 * kvals["Nx"]:]
+    Nx = kvals["Nx"]
+    # J = U[:Nx]
+    # M = U[Nx:2 * Nx]
+    # A = U[2 * Nx:3 * Nx]
+    # P = U[3 * Nx:]
     fig, ax = plt.subplots()
-    line1, = ax.plot(kvals["X"], sol.y[:kvals["Nx"], 0]/scalar, label="par3", color="green")
-    line2, = ax.plot(kvals["X"], sol.y[kvals["Nx"]:2*kvals["Nx"], 0]/scalar, label="anterior", color="blue")
-    line3, = ax.plot(kvals["X"], sol.y[2*kvals["Nx"]:, 0]/scalar, label="posterior", color="orange")
+    line1, = ax.plot(kvals["X"], sol.y[:Nx, 0]/scalar, label="par3", color="green")
+    line2, = ax.plot(kvals["X"], sol.y[Nx:2*Nx, 0]/scalar, label="par3-PKC", color="purple")
+    line3, = ax.plot(kvals["X"], sol.y[2*Nx:3*Nx, 0]/scalar, label="cdc42-PKC", color="blue")
+    line4, = ax.plot(kvals["X"], sol.y[3*Nx:, 0]/scalar, label="posterior", color="orange")
     p_m = 0 #polarity_measure(kvals["X"], sol.y[:kvals["Nx"], 0], sol.y[kvals["Nx"]:, 0], kvals["Nx"])
     time_label = ax.text(0.1, 1.05, f"t={sol.t[0]} p={p_m:.4f}", transform=ax.transAxes, ha="center")
     linev, = ax.plot(kvals["X"], [v_rescale_for_visibility*kvals["v_func"](kvals, x, 0) for x in kvals["X"]], label="v", linestyle="--", color="black")
@@ -198,9 +220,10 @@ def animate_plot(sol, kvals: dict, save_file=False, file_code: str = None, resca
 
     def animate(t_i):
         linev.set_ydata([v_rescale_for_visibility*kvals["v_func"](kvals, x, sol.t[t_i]) for x in kvals["X"]])
-        line1.set_ydata(sol.y[:kvals["Nx"], t_i]/scalar)
-        line2.set_ydata(sol.y[kvals["Nx"]:2*kvals["Nx"], t_i]/scalar)
-        line3.set_ydata(sol.y[2*kvals["Nx"]:, t_i]/scalar)
+        line1.set_ydata(sol.y[:Nx, t_i]/scalar)
+        line2.set_ydata(sol.y[Nx:2*Nx, t_i]/scalar)
+        line3.set_ydata(sol.y[2*Nx:3*Nx, t_i]/scalar)
+        line4.set_ydata(sol.y[3*Nx:, t_i]/scalar)
         p_m = 0 #polarity_measure(kvals["X"], sol.y[:kvals["Nx"], t_i], sol.y[kvals["Nx"]:, t_i], kvals["Nx"])
         time_label.set_text(f"t={sol.t[t_i]:.2f} p={p_m:.4f}")
         return (line1, line2, line3, linev, time_label)
--- src/tasks/run_par3addition.py
+++ src/tasks/run_par3addition.py
@@ -1,31 +1,27 @@
-# # https://stackoverflow.com/questions/17053671/how-do-you-stop-numpy-from-multithreading
-# import os
-# os.environ['OPENBLAS_NUM_THREADS'] = '1'
-#
-# from src import model_task_handler
-# from matplotlib import pyplot as plt
-# from ..models import MODELS, model_to_module
-#
-#
-# model_module = model_to_module(MODELS.PAR3ADD)
-#
-#
-# if __name__ == '__main__':
-#     TASKS = [(MODELS.PAR3ADD, {"points_per_second": 0.1, "tL": 1600, "Nx":100,
-#                "initial_condition":[0.1]*100+[0.5]*100+[0]*30+[0]*70,
-#                # "D_A": 0.35, "D_J": 0.1,
-#                "rho_A": 0.3, "rho_J": 0.3, "rho_P": 2,
-#                # "sigma_P": 0,
-#                "k_PA": 2, "k_AJ": 2
-#            })]
-#
-#     results = model_task_handler.run_tasks_parallel(TASKS)
-#
-#     for res in results:
-#         if res[1] == "FAILURE":
-#             continue
-#
-#         model_module.animate_plot(res[1], res[2], save_file=False, rescale=False)
-#
-#     plt.show()
-#
+# https://stackoverflow.com/questions/17053671/how-do-you-stop-numpy-from-multithreading
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
+from src import model_task_handler
+from matplotlib import pyplot as plt
+from ..models import MODELS, model_to_module
+
+
+model_module = model_to_module(MODELS.PAR3ADD)
+
+
+if __name__ == '__main__':
+    TASKS = [(MODELS.PAR3ADD, {"points_per_second": 0.1, "tL": 1000, "Nx":100,
+                "initial_condition": [0]*100 + [0]*100 + [0]*100 + [0]*100,
+                })]
+
+    results = model_task_handler.run_tasks_parallel(TASKS)
+
+    for res in results:
+        if res[1] == "FAILURE":
+            continue
+
+        model_module.animate_plot(res[1], res[2], save_file=False, rescale=False)
+
+    plt.show()
+
