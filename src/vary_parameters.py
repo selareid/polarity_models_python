4e89ab23fe89450ddcf3317781bf687da20770d4
Daniel <drei509@aucklanduni.ac.nz>
2024-02-15 16:35:55 +1300

    update

--- src/vary_parameters.py
+++ src/vary_parameters.py
@@ -1,4 +1,3 @@
-import time
 # https://stackoverflow.com/questions/17053671/how-do-you-stop-numpy-from-multithreading
 import os
 os.environ['OPENBLAS_NUM_THREADS'] = '1'
@@ -8,12 +7,18 @@ from models import MODELS, model_to_module
 import model_task_handler
 
 def a_func(kvals, lt, x): return 0
-def v_func(kvals, x, t):
+def v_func_old(kvals, x, t):
     # return np.minimum(0.7, np.maximum(0, x - 35)) / (np.max([t, 100]) - 99) ** 3
-    # return -x*(x-67.3)*(x-30) / (50000*(np.maximum(1,np.abs((t-200)/150)))**2)
+    return -x*(x-67.3)*(x-30) / (50000*(np.maximum(1,np.abs((t-200)/150)))**2)
+    # return 0
+
+def v_func_zero(kvals, x, t):
     return 0
 
+
 # task format is tuples like (model, args) in a list
+# TASKS = [(MODELS.TOSTEVIN, {"points_per_second": 0.1, "tL": 1000, "Nx":100, "initial_condition":[0]*200})]
+
 # TASKS = [(MODELS.TOSTEVIN, {"points_per_second": 1, "tL": 1000, "sort": 0.25, "Nx": 100}),
 #     (MODELS.TOSTEVIN, {"label": "Dm=0.1", "points_per_second": 1, "tL": 1000, "Dm": 0.1, "sort": 0.1, "Nx": 100}),
 #     (MODELS.TOSTEVIN, {"label": "Dm=0.2", "points_per_second": 1, "tL": 1000, "Dm": 0.2, "sort": 0.2, "Nx": 100}),
@@ -24,49 +29,60 @@ def v_func(kvals, x, t):
 #     (MODELS.TOSTEVIN, {"label": "Dm=5", "points_per_second": 1, "tL": 1000, "Dm": 5, "sort": 5, "Nx": 100}),
 #     (MODELS.TOSTEVIN, {"label": "Dm=10", "points_per_second": 1, "tL": 1000, "Dm": 10, "sort": 10, "Nx": 100}),]
 
-TASKS = [(MODELS.GOEHRING, {"points_per_second": 1, "tL": 1000, "Nx": 300}),]
+# TASKS = [(MODELS.GOEHRING, {"points_per_second": 1, "tL": 1000, "Nx": 100}),]
 
-# TASKS = [(MODELS.GOEHRING, {"points_per_second": 1, "tL": 3600, "v_func": v_func}),]
+Nx=100
+TASKS = [(MODELS.GOEHRING, {"points_per_second": 3, "tL": 1, "Nx":100, "initial_condition": [0] * (Nx // 2) + [1] * (Nx - Nx // 2) + [1] * (Nx // 2) + [0] * (Nx - Nx // 2),
+    "label":"maintenance IC", "v_func": v_func_zero})]
+
+
 
 # TASKS = [(MODELS.GOEHRING, {"label": "Dp=0.5", "points_per_second": 1, "tL": 300, "D_P": 0.5, "Nx": 300}),
 #          (MODELS.GOEHRING, {"label": "Dp=0.2", "points_per_second": 1, "tL": 300, "D_P": 0.2, "Nx": 300}),
 #          (MODELS.GOEHRING, {"label": "Dp=0.1", "points_per_second": 1, "tL": 300, "D_P": 0.1, "Nx": 300}),
 #          (MODELS.GOEHRING, {"label": "Dp=0.01", "points_per_second": 1, "tL": 300, "D_P": 0.01, "Nx": 300}),]
 
+if __name__ == '__main__':
+    # freeze_support()
 
-results = model_task_handler.run_tasks_parallel(TASKS)
+    results = model_task_handler.run_tasks_parallel(TASKS)
 
-results.sort(key=lambda res: 0 if res[1]=="FAILURE" or not "sort" in res[2] else res[2]["sort"])  # order when plotting multiple solutions on single figure
+    results.sort(key=lambda res: 0 if res[1]=="FAILURE" or not "sort" in res[2] else res[2]["sort"])  # order when plotting multiple solutions on single figure
 
-sol_list = []
-kvals_list = []
+    sol_list = []
+    kvals_list = []
 
-for res in results:
-    if res[1] == "FAILURE":
-        continue
+    for res in results:
+        if res[1] == "FAILURE":
+            continue
 
-    model_module = model_to_module(res[0])
+        model_module = model_to_module(res[0])
 
-    ### Plots and Animated Plots for individual solutions
-    model_module.plot_overall_quantities_over_time(res[1], res[2])
-    model_module.plot_final_timestep(res[1], res[2])
-    model_module.animate_plot(res[1], res[2], save_file=False)
+        ### Plots and Animated Plots for individual solutions
+        # model_module.plot_overall_quantities_over_time(res[1], res[2])
+        # model_module.plot_final_timestep(res[1], res[2])
+        model_module.animate_plot(res[1], res[2], save_file=True, rescale=False)
+        # model_module.plot_lt(res[1], res[2])
 
-    sol_list.append(res[1])
-    kvals_list.append(res[2])
+        print(f"Last time: A={res[1].y[0, -1]} P={res[1].y[-1, -1]}")
 
-### Below Outputs Final Timestep for All Solutions ###
-# it makes some assumptions about the input e.g. all same shape for time/space, that all inputs are relevant, ...
-### For tostevin
-# model_module = model_to_module(MODELS.TOSTEVIN)
-# model_module.plot_multi_final_timestep(sol_list, kvals_list, plot_Ac=False,plot_Pm=False,plot_Pc=False)
-# model_module.plot_multi_final_timestep(sol_list, kvals_list, plot_Am=False,plot_Pm=False,plot_Pc=False)
-# model_module.plot_multi_final_timestep(sol_list, kvals_list, plot_Am=False,plot_Ac=False,plot_Pc=False)
-# model_module.plot_multi_final_timestep(sol_list, kvals_list, plot_Am=False,plot_Ac=False,plot_Pm=False)
-# model_module.plot_multi_final_timestep(sol_list, kvals_list)
-### For goehring
-# model_module = model_to_module(MODELS.TOSTEVIN)
-# model_module.plot_multi_final_timestep(sol_list, kvals_list)
+        sol_list.append(res[1])
+        kvals_list.append(res[2])
 
 
-plt.show()  # if we don't have this the program ends and the plots close
+
+    ### Below Outputs Final Timestep for All Solutions ###
+    # it makes some assumptions about the input e.g. all same shape for time/space, that all inputs are relevant, ...
+    ### For tostevin
+    # model_module = model_to_module(MODELS.TOSTEVIN)
+    # model_module.plot_multi_final_timestep(sol_list, kvals_list, plot_Ac=False,plot_Pm=False,plot_Pc=False)
+    # model_module.plot_multi_final_timestep(sol_list, kvals_list, plot_Am=False,plot_Pm=False,plot_Pc=False)
+    # model_module.plot_multi_final_timestep(sol_list, kvals_list, plot_Am=False,plot_Ac=False,plot_Pc=False)
+    # model_module.plot_multi_final_timestep(sol_list, kvals_list, plot_Am=False,plot_Ac=False,plot_Pm=False)
+    # model_module.plot_multi_final_timestep(sol_list, kvals_list)
+    ### For goehring
+    # model_module = model_to_module(MODELS.TOSTEVIN)
+    # model_module.plot_multi_final_timestep(sol_list, kvals_list)
+
+
+    plt.show()  # if we don't have this the program ends and the plots close
