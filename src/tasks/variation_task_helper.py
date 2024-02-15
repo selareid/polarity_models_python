ac8b3520e9cb0bf00ed08632e7cb6ac8c8a472ca
Daniel <drei509@aucklanduni.ac.nz>
2024-02-15 16:32:54 +1300

    update

--- src/tasks/variation_task_helper.py
+++ src/tasks/variation_task_helper.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 from src import model_task_handler
 from src.models import MODELS, model_to_module
 
 
 # standard variation multipliers
-def get_variation_multiplier(option=0):
+def get_variation_multiplier(option=None):
     # variation_multipliers = [0.1, 0.25, 0.5, 2, 5, 10] # wide
     # variation_multipliers = [0.7, 0.8, 0.9, 0.95, 1.05, 1.1, 1.2, 1.3]  # narrow
     # if option == 1: # middle
@@ -12,34 +14,46 @@ def get_variation_multiplier(option=0):
     # else:  # option == 0 and others; default variation
     # variation_multipliers = [0.1, 0.25, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.975, 1.025, 1.05, 1.1, 1.2, 1.3, 1.5, 1.75, 2, 5, 10]
     # index_for_100x = 9
-    import numpy as np
-    variation_multipliers = np.arange(0.5, 1.5, 0.05).tolist() +[0.01,0.05,0.1,0.25] + [1.75,2,5,10]
+    if option == "scribble":
+        # variation_multipliers = [0.65, 0.8, 1.35, 1.7]
+        variation_multipliers = (np.linspace(0.5, 1.80, 9).tolist()
+                                 + np.linspace(0,0.5,4).tolist()
+                                 + np.linspace(1.8,5,4).tolist()
+                                 + np.linspace(5,10,7).tolist())
+        # variation_multipliers = np.linspace(4, 5.8, 10).tolist()# + [0.01, 10]
+        variation_multipliers.append(1)
+    elif option == "sparse":
+        variation_multipliers = np.arange(0.5, 1.5 + 0.5, 0.5).tolist() + [0, 0.01, 0.05, 0.1, 0.25] + [1.75, 2, 5, 10]
+    else:
+        variation_multipliers = np.arange(0.5, 1.5 + 0.05, 0.05).tolist() +[0, 0.01,0.05,0.1,0.25] + [1.75,2,5,10]
+
     variation_multipliers.sort()
-
     index_for_100x = np.searchsorted(variation_multipliers, 1, 'right')
-
     return variation_multipliers, index_for_100x
+
 
 def get_xticks(variation_multipliers, index_for_100x):
     xticks = [format(f"{x * 100:.2f}").rstrip('0').rstrip('.')+"%" for x in variation_multipliers]
     xticks.insert(index_for_100x, "100%")
     return xticks
 
+
 def generate_tasks(model_type: MODELS, variations_list: list[float], default_params: dict, other_param_vals: dict={}) -> list[list[tuple]]:
     sort_i = 1
 
     baseline_params = {**default_params, **other_param_vals}
-    tasks: list[list(tuple)] = [[(model_type, {**baseline_params, "sort": sort_i})]]  # we add baseline task
+    tasks: list[list(tuple)] = [[(model_type, {**baseline_params, "sort": sort_i, "variation_multiplier": 1})]]  # we add baseline task
     sort_i += 1
 
     for key in default_params.keys():
         task_list = []
 
-        if key not in ["key_varied", "label", "points_per_second", "Nx", "L", "x0", "xL", "t0", "tL", "v_func", "a_func"]:  # exclude run conditions
+        if key not in ["key_varied", "variation_multiplier", "label", "points_per_second", "Nx", "L", "x0", "xL", "t0", "tL", "v_func", "a_func"]:  # exclude run conditions
             for multiplier in variations_list:
                 task_list.append((model_type, {**baseline_params,
-                    "label": format(f"{key}={baseline_params[key] * multiplier:.2f}"),
-                    "key_varied": format(f"{key}"), key: baseline_params[key] * multiplier, "sort": sort_i}))
+                    "label": format(f"{key}={baseline_params[key] * multiplier:.4f}"),
+                    "key_varied": format(f"{key}"), key: baseline_params[key] * multiplier,
+                    "variation_multiplier": multiplier, "sort": sort_i}))
 
                 sort_i += 1
 
@@ -95,13 +109,14 @@ def split_baseline_from_results(model_type: MODELS, all_results_by_variable: lis
                 has_failure = True
             #     continue
             elif res[2]["label"] in extra_plot:
-                model_to_module(model_type).plot_final_timestep(res[1], res[2], rescale=True)
-                model_to_module(model_type).animate_plot(res[1], res[2], rescale=True, save_file=True, file_code=format(f"{res[2]['label']},N={res[2]['Nx']},T={res[2]['tL']}"))
+                model_to_module(model_type).plot_final_timestep(res[1], res[2], rescale=False)
+                model_to_module(model_type).animate_plot(res[1], res[2], rescale=False, save_file=True, file_code=format(f"{res[2]['label']},N={res[2]['Nx']},T={res[2]['tL']}"))
 
 
             if is_baseline:
                 baseline_result = (res[1], res[2])
-                model_to_module(model_type).animate_plot(res[1], res[2], rescale=True)
+                # model_to_module(model_type).animate_plot(res[1], res[2], rescale=True)
+                
 
             sol_list.append(res[1])
             kvals_list.append(res[2])
