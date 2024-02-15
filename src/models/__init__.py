6aec492f8c07023ea884880ec2fb6b6796f5a96a
Daniel <drei509@aucklanduni.ac.nz>
2024-02-15 16:21:27 +1300

    updated models list

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,12 @@
 from enum import Enum
-from . import goehring, tostevin
+from . import goehring, tostevin, par3addition, crumbs
 
 
 class MODELS(Enum):
     TOSTEVIN = 0
     GOEHRING = 1
+    PAR3ADD = 2
+    CRUMBS = 3
 
 
 def model_to_module(model: MODELS):
@@ -13,5 +15,9 @@ def model_to_module(model: MODELS):
             return tostevin
         case MODELS.GOEHRING:
             return goehring
+        case MODELS.PAR3ADD:
+            return par3addition
+        case MODELS.CRUMBS:
+            return crumbs
         case _:
             raise ValueError(f"Unexpected model value: {model}")
