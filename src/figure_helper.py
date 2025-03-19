f482b375ef6cd64e991862e0b3ff3223f2bdd217
Daniel Reid <drei509@aucklanduni.ac.nz>
2025-03-19 15:04:27 +1300

    changed legend labels for par3add and added x/y-axis labels

--- src/figure_helper.py
+++ src/figure_helper.py
@@ -13,5 +13,10 @@ par3add_colours = ["green", "purple", "blue", "orange"]
 
 # labels
 goehring_labels = ["aPar", "pPar"]
-par3add_labels = ["Par3", "Par3-Par6-PKC",
-                  "CDC42-Par6-PKC", "Posterior"]
+# par3add_labels = ["Par3 ($J$)", "Par3-Par6-PKC ($M$)",
+#                   "CDC42-Par6-PKC ($A$)", "Posterior ($P$)"]
+par3add_labels = ["$J$ (Par3)", "$M$ (Par3-Par6-PKC)",
+                  "$A$ (CDC42-Par6-PKC)", "$P$ (Posterior)"]
+
+xlabel = "μm"
+ylabel = r"$\text{μm}^{-2}$"
