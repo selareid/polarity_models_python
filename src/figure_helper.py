f0d8aaaa3f004c391cf17f26de0af124ecf50cf4
Daniel Reid <drei509@aucklanduni.ac.nz>
2025-03-27 12:54:42 +1300

    tidying up

--- src/figure_helper.py
+++ src/figure_helper.py
@@ -1,6 +1,6 @@
-# universal thing for figures
-# ordering for par3add is [J, M, A, P]
-# for goehring is just [A, P]
+# Universal thing for figures
+# ordering for par3add elements is [J, M, A, P]
+# for goehring the ordering is just [A, P]
 
 
 font_size = 17
@@ -13,8 +13,6 @@ par3add_colours = ["green", "purple", "blue", "orange"]
 
 # labels
 goehring_labels = ["aPar", "pPar"]
-# par3add_labels = ["Par3 ($J$)", "Par3-Par6-PKC ($M$)",
-#                   "CDC42-Par6-PKC ($A$)", "Posterior ($P$)"]
 par3add_labels = ["$J$ (Par3)", "$M$ (Par3-Par6-PKC)",
                   "$A$ (CDC42-Par6-PKC)", "$P$ (Posterior)"]
 
